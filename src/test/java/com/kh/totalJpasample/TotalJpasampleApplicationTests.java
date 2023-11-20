136b77fc5baf8375656fc34d995a049cba9b0b63
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-11-20 11:32:47 +0900

    Initial commit

---
+++ src/test/java/com/kh/totalJpasample/TotalJpasampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.totalJpasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalJpasampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
